36fa3a76c6c14ff63e80761588662d8761d62f49
Fabiano Almeida <fabianoalmeidasjc@gmail.com>
2020-06-17 17:09:02 -0300

    Cria a tradução de código para texto, TODO: teclado alfamorse morse separar as letras em morse, feita no texto

--- morse/src/app/pages/morse/morse.page.ts
+++ morse/src/app/pages/morse/morse.page.ts
@@ -9,6 +9,8 @@ export class MorsePage implements OnInit {
 
   codigoMorse: string = ''
   texto: string = ''
+  codigoTraduzido: string = ''
+  codigo: string = ''
   tabelaMorse: object = {'A': '.-', 'À': '.-', 'Á': '.-', 'Â': '.-',
                          'B': '-...', 'C': '-.-.', 'Ç': '-.-.',
                          'D': '-..', 'E': '.', 'É': '.', 'Ê': '.',
@@ -23,10 +25,30 @@ export class MorsePage implements OnInit {
                          '1': '.----', '2': '..---', '3': '...--',
                          '4': '....-', '5': '.....', '6': '-....',
                          '7': '--...', '8': '---..', '9': '----.', '0': '-----'}
+  tabelaTexto: object = {".-": "A", "-...": "B", "-.-.": "C",
+                         "-..": "D", ".": "E",
+                         "..-.": "F", "--.": "G", "....": "H", "..": "I",
+                         ".---": "J", "-.-": "K", ".-..": "L",
+                         "--": "M", "-.": "N", "---": "O", 
+                         ".--.": "P", "--.-": "Q", ".-.": "R",
+                         "...": "S", "-": "T", "..-": "U",
+                         "...-": "V", ".--": "W", "-..-": "X",
+                         "-.--": "Y", "--..": "Z", 
+                         " ": " ",
+                         ".----": "1", "..---": "2", "...--": "3",
+                         "....-": "4", ".....":"5", "-....": "6",
+                         "--...": "7", "---..": "8", "----.": "9", "-----": "0"}
+  opcaoSelecionada : string = 'texto'
   constructor() { }
 
   ngOnInit() {
   }
+
+  segmentChanged(event){
+    console.log(event)
+    this.opcaoSelecionada = event.detail.value
+  }
+
 
   codificar(texto: string){
     let aCodificar = texto.toUpperCase()
@@ -38,9 +60,28 @@ export class MorsePage implements OnInit {
     this.codigoMorse = codigo;
     console.log("result: ", this.codigoMorse)
   }
+  decodificar(code: string){
+    let deCodificar = code.split(" ")
+    let texto = ''
+    console.log("deCodar", deCodificar)
+    for (let index = 0; index < deCodificar.length; index++) {
+      let temp = this.tabelaTexto[deCodificar[index]];
+      if (temp == undefined) {
+        temp = " "
+      }
+      texto += temp;
+    }
+    this.codigoTraduzido = texto;
+    console.log("result: ", this.codigoTraduzido)
+  }
 
-  limpar(){
+  limparCodigo(){
     this.codigoMorse = ''
     this.texto = ''
   }
+  limparTexto(){
+    this.codigoTraduzido = ''
+    this.codigo = ''
+  }
+
 }
